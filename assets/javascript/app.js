2ad55e44381889a8da5ab9954f62198ca8d8ffe0
Megan Lockette <megan3693@sbcglobal.net>
2019-11-17 19:05:54 -0800

    editing html and adding firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA1fQ-V-M3zJtNGoK0sAsDNcE5Qqy8bV44",
+    authDomain: "train-schedule-ec948.firebaseapp.com",
+    databaseURL: "https://train-schedule-ec948.firebaseio.com",
+    projectId: "train-schedule-ec948",
+    storageBucket: "train-schedule-ec948.appspot.com",
+    messagingSenderId: "107998113575"
+  };
+  
+  // Initialize Firebase
+  
+  
+firebase.initializeApp(firebaseConfig);
+  
+
+
+
+
